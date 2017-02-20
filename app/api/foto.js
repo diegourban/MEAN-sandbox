4320db69abf536870de3d3a5aafb27df5f8c3368
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-02-20 13:22:09 -0300

    find by id

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,12 +1,20 @@
 var api = {};
 
-api.lista = function(req, res) {
-  var fotos = [
-    {_id: 1, titulo: 'Leão 1', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
-    {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
-  ];
+var fotos = [
+  {_id: 1, titulo: 'Leão 1', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
+  {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
+];
 
+api.lista = function(req, res) {
   res.json(fotos);
+};
+
+api.buscaPorId = function(req, res) {
+  var fotoEncontrada = fotos.find(function(foto) {
+    return foto._id == req.params.id;
+  });
+
+  res.json(fotoEncontrada);
 };
 
 module.exports = api;
